fba920d73e4e3d21da95b6dd1e9aaa546dbad764
Kayetic <bartek.bak@protonmail.com>
2023-08-31 20:21:44 +0100

    added a POST direction

--- temp/back-end/app.py
+++ temp/back-end/app.py
@@ -1,18 +1,31 @@
 from flask import Flask
+from flask import request, jsonify
+
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/greet')
+
+@app.route("/greet")
 def greet():
-    return 'Hello again!'
+    return "Hello again!"
 
-@app.route('/user/<username>')
+
+@app.route("/user/<username>")
 def show_user_profile(username):
-    return f'User {username}'
+    return f"User {username}"
 
-@app.route('/api/events', methods=['GET'])
+
+@app.route("/api/events", methods=["GET"])
 def get_events():
     return {"event1": "Birthday", "event2": "Meeting"}
+
+
+@app.route("/api/events", methods=["POST"])
+def add_event():
+    event = request.json["event"]
+    return jsonify({"message": f"Event {event} added"}), 201
