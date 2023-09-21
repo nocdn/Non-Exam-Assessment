f977c6ef49940167309af90ed11a6c57e181eb8a
Kayetic <bartek.bak@protonmail.com>
2023-09-21 09:29:37 +0100

    🔥 refactor(api.py): remove commented out code for /api/events route to improve code readability and maintainability

--- api/api.py
+++ api/api.py
@@ -7,13 +7,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-# @app.route("/api/events", methods=["GET"])
-# def get_events():
-#     with open("api/events/2023/events-9.json", "r") as f:
-#         events = json.load(f)
-#     return jsonify(events)
-
-
 @app.route("/api/events/<year>/<month>", methods=["GET"])
 def get_events(year, month):
     file_path = os.path.join("api/events", year, f"events-{month}.json")
