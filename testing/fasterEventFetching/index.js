1d1ad37e7714925a40507fa96f5486211b92efd7
Kayetic <bartek.bak@protonmail.com>
2024-02-13 14:27:08 +0000

    implemented a way to fetch events directly from s3, 6x faster than through lambda, near instant

---
+++ testing/fasterEventFetching/index.js
@@ -0,0 +1,32 @@
+const fetchButton = document.querySelector(".fetch-btn");
+const eventList = document.querySelector(".event-list");
+const inputField = document.querySelector(".group-input");
+
+let xmlString = null;
+const fetchKeys = async function (group_id) {
+  const url = "https://shared-calendar-bucket.s3.amazonaws.com/?prefix=1234/";
+  // fetch that then wait for the response, and then set the respons to the variable xml string
+  const response = await fetch(url);
+  xmlString = await response.text();
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+
+  const fetchFiles = async function (key) {
+    const url = `https://shared-calendar-bucket.s3.amazonaws.com/${key}`;
+    const response = await fetch(url);
+    const text = await response.text();
+    console.log(text);
+  };
+
+  // Extracting <Key> elements
+  const keys = xmlDoc.getElementsByTagName("Key");
+  for (let i = 0; i < keys.length; i++) {
+    console.log(keys[i].childNodes[0].nodeValue);
+    const key = keys[i].childNodes[0].nodeValue;
+    fetchFiles(key);
+  }
+};
+
+fetchButton.addEventListener("click", function () {
+  fetchKeys(inputField.value);
+});
